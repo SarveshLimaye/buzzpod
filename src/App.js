1539cc8d9516fb45f90f41a611d9aea84cc25cc0
SarveshLimaye <sarveshlimaye2002@gmail.com>
2023-02-18 16:05:04 +0530

    routes set up done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Podcasts from "./pages/Podcasts/Podcasts";
+import AddPodcast from "./pages/AddPodcast/AddPodcast";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/podcasts" element={<Podcasts />} />
+        <Route path="/add" element={<AddPodcast />} />
+      </Routes>
+     </Router>
     </div>
   );
 }
---
+++ src/pages/AddPodcast/AddPodcast.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function AddPodcast() {
+  return (
+    <div>AddPodcast</div>
+  )
+}
