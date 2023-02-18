5655bcfdcfc1eae28b5b098a44eb9895eb52882b
SarveshLimaye <sarveshlimaye2002@gmail.com>
2023-02-18 17:05:59 +0530

    navbar + hero added

--- src/index.js
+++ src/index.js
@@ -3,11 +3,35 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+
+const colors = {
+  brand: {
+    50: "#ecefff",
+    100: "#cbceeb",
+    200: "#a9aed6",
+    300: "#888ec5",
+    400: "#666db3",
+    500: "#4d5499",
+    600: "#3c4178",
+    700: "#2a2f57",
+    800: "#181c37",
+    900: "#080819"
+  }
+};
+const config = {
+  initialColorMode: "dark",
+  useSystemColorMode: false
+};
+
+const theme = extendTheme({ colors, config })
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+  <ChakraProvider theme={theme}>
     <App />
+    </ChakraProvider>
   </React.StrictMode>
 );
 
